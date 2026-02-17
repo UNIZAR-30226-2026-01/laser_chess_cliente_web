0f4ab1bc39d7bfa9004b86aca7451be702906281
ainhoacrb <a901808@unizar.es>
2026-02-17 16:02:39 +0100

    Correción nombre en archivos ts

--- laser-chess-web/src/app/app.ts
+++ laser-chess-web/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('mi-primer-proyecto');
+  protected readonly title = signal('laser-chess-web');
 }
