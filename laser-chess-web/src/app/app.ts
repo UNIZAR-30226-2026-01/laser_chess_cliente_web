1a369c5859d41f36094f6cd49721a385f1dabb07
PaulaMelero <901449@unizar.es>
2026-02-18 11:57:54 +0100

    Compontentes de Juego e Inicio añadidos, pruebas de navegación

--- laser-chess-web/src/app/app.ts
+++ laser-chess-web/src/app/app.ts
@@ -1,10 +1,13 @@
+// Cerebro del componente principal de la app
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
+  // Enlaza con app.html > página principal de la app
   templateUrl: './app.html',
+  // Enlaze con app.css > estilos globales de la app
   styleUrl: './app.css'
 })
 export class App {
