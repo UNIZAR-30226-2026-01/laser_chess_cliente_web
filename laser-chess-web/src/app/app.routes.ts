ff63a69e604536c8af1d588ae1273a2f18b50e8a
PaulaMelero <901449@unizar.es>
2026-02-18 12:29:52 +0100

    Navegación básica, página de Inicio, Juego conectadas, Perfil Closes #2

--- laser-chess-web/src/app/app.routes.ts
+++ laser-chess-web/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './inicio/inicio';
 import { Juego } from './juego/juego';
+import { Perfil } from './perfil/perfil';
 
 // Aquí se deben añadir objetos que conectan una ruta con un componente fijo
 export const routes: Routes = [
     { path: 'inicio', component: Inicio },
-    { path: 'juego', component: Juego }
+    { path: 'juego', component: Juego },
+    { path: 'perfil', component: Perfil }
 ];
