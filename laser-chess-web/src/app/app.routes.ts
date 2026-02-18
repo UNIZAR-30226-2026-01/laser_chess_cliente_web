6ef2da5154747fec6867ee7d1d8e04e0813561ac
ainhoacrb <a901808@unizar.es>
2026-02-18 12:49:05 +0100

    Navegación básica desde index.html probada y funcional

--- laser-chess-web/src/app/app.routes.ts
+++ laser-chess-web/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Game } from './features/game/game';
+import { Result } from './features/result/result';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'game', component: Game },
+    { path: 'result', component: Result },
+];
