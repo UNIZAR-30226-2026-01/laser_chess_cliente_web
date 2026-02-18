c46bc66e80c3c20e3edfc2d7c27dcef1787f8ec6
PaulaMelero <901449@unizar.es>
2026-02-18 21:39:22 +0100

    Commit del proyecto previo al inicio de implementación de funcionalidad de login / sign in

--- laser-chess-web/src/app/app.routes.ts
+++ laser-chess-web/src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { Game } from './features/game/game';
 import { Result } from './features/result/result';
+import { Profile } from './features/profile/profile';
 
+// Define las rutas de la aplicación, cada ruta asocia una URL con un componente específico
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'game', component: Game },
     { path: 'result', component: Result },
+    { path: 'profile', component: Profile },
 ];
 
