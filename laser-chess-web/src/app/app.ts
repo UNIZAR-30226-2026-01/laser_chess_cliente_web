e5794b21c6d917e19ef01405bfa6752dd212af13
ainhoacrb <a901808@unizar.es>
2026-02-19 02:21:16 +0100

    correción import no utilizado

--- laser-chess-web/src/app/app.ts
+++ laser-chess-web/src/app/app.ts
@@ -1,11 +1,11 @@
 // Cerebro del componente principal de la app
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './shared/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLinkWithHref, Sidebar],
+  imports: [RouterOutlet, Sidebar],
 
   templateUrl: './app.html',
   // Enlaze con app.css > estilos globales de la app
