92ad18dfc3a33b090eff434ae53350aed33e4b74
ainhoacrb <a901808@unizar.es>
2026-02-22 23:27:55 +0100

    HOME: estructura general lista

--- laser-chess-web/src/app/features/home/home.ts
+++ laser-chess-web/src/app/features/home/home.ts
@@ -9,6 +9,8 @@ import { Component } from '@angular/core';
 export class Home {
   username = 'User';
   pictureURL = '/assets/picture.jpeg';
+  timeModeLabel = 'Modo de tiempo';
+  boardPreviewUrl = '/assets/picture.jpeg';
   coins = 1234;
   rankedPoints = 1234;
 }
