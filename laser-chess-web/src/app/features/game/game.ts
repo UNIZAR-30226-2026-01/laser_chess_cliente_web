e19158c0f363924c2842dadedc06ba20535d9c02
ainhoacrb <901808@unizar.es>
2026-02-24 14:21:43 +0100

    GAME en proceso, pantallas principales funcionando correctamente

--- laser-chess-web/src/app/features/game/game.ts
+++ laser-chess-web/src/app/features/game/game.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
-type Helix = 'red' | 'white' | null;
+type Helix = 'red' | 'blue' | null;
 
 interface Cell {
-  r: number;
-  c: number;
-  dark: boolean;
-  coord: string;
+  f: number;        // num. de filas
+  c: number;        // num. de cols
+  coord: string;    // tipo ajedrez: A1
   helix: Helix;
-  corner: boolean;
+  corner: boolean;  // es una esquina?
 }
 
-const FILE = 'ABCDEFGH';
+const COLS = 'ABCDEFGHIJ';
 
 @Component({
   selector: 'app-game',
@@ -21,4 +20,30 @@ const FILE = 'ABCDEFGH';
 })
 export class Game implements OnInit {
 
+  readonly rows = 8;
+  readonly cols = 10;
+
+  cells: Cell[] = [];
+
+  ngOnInit(): void {
+    this.cells = this.buildBoard(this.rows, this.cols);
+  }
+
+  trackCell = (_: number, cell: Cell) => '${cell.f}-${cell.c}';
+
+  private buildBoard(rows: number, cols: number): Cell[] {
+    const cells: Cell[] = [];
+
+    for (let r = 0; r < rows; r++) {
+      for (let c = 0; c < cols; c++) {
+        const coord = this.toCoord(r, c, rows);
+
+        const corner = (r == 0 || r == rows - 1) && (c == 0 || c == cols - 1);
+
+        const helix = this.helixAt(r, c, rows, cols);
+
+        cells.push({r, c, coord, helix, corner});
+      }
+    }
+  }
 }
