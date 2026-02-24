6e76e75e07afd3378adbe5dc5baeed7d3efca3ef
PaulaMelero <901449@unizar.es>
2026-02-24 14:23:41 +0100

    ultima versión rama_paula

--- laser-chess-web/src/app/app.ts
+++ laser-chess-web/src/app/app.ts
@@ -1,22 +1,11 @@
 // Cerebro del componente principal de la app
 import { Component, signal } from '@angular/core';
-<<<<<<< HEAD
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './shared/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Sidebar],
-
-=======
-import { RouterOutlet, RouterLink } from '@angular/router';
-import { Dashboard } from './pages/dashboard/dashboard';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, Dashboard],
-  
->>>>>>> 45a80906dec0f0d1b72e24cffacce11e9282e498
   templateUrl: './app.html',
   // Enlaze con app.css > estilos globales de la app
   styleUrl: './app.css'
