7deeddb11ab3a0af6f36f7ebf2ff10a15f27a76a
PaulaMelero <901449@unizar.es>
2026-03-05 12:53:10 +0100

    Avance en pantalla de SOCIAL, actualización de varios css para hacer responsive toda la interfaz, #24 #25

--- laser-chess-web/src/app/features/social/social.ts
+++ laser-chess-web/src/app/features/social/social.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-social',
@@ -7,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './social.css',
 })
 export class Social {
+  username = 'User';
+  pictureURL = '/assets/picture.jpeg';
+  timeModeLabel = 'Modo de tiempo';
+  boardPreviewUrl = '/assets/picture.jpeg';
+  coins = 1234;
+  rankedPoints = 1234;
+  public popUP_nuevoAmigo = signal(false);
 
+  onArrowClick() {
+    // Llamada al backend para obetener solicitudes de amistad?
+    console.log('Arrow clicked');
+    // Redirigir a pantalla de solicitudes de amistad
+  }
+
+  nuevoAmigo(){
+    console.log('Abrir pop-up para introducir datos de nuevo amigo');
+    this.popUP_nuevoAmigo.set(true);
+  }
+
+  copyLink(){}
 }
