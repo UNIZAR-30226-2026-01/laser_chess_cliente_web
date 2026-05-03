5fa639d60fa902b6e267dd22291fc52f77730b97
MimiMc4 <miguelmarclave@gmail.com>
2026-05-03 13:28:01 +0200

    runas añadidas

--- laser-chess-web/src/app/shared/board/board.ts
+++ laser-chess-web/src/app/shared/board/board.ts
@@ -4,6 +4,7 @@ import { PiezaRival } from '../../shared/pieza-rival/pieza-rival';
 import { Laser } from '../../shared/laser/laser'
 import { PiezaData } from '../../model/game/PiezaData';
 import { GameState } from '../../utils/game-state';
+import { BoardState } from '../../utils/board-state';
 
 
 @Component({
@@ -15,6 +16,7 @@ import { GameState } from '../../utils/game-state';
 
 export class Board {
   state = inject(GameState);
+  boardState = inject(BoardState);
 
   @Input() piezas!: Signal<PiezaData[]>;
   @Input() laserPath!: Signal<{x:number,y:number}[]>;
@@ -28,4 +30,21 @@ export class Board {
   @Input() isCasillaRestringida!: (x: number, y: number) => 'azul' | 'rojo' | null;
   @Input() ocupado!: (x: number, y: number) => PiezaData | null;
 
+  getRuneUrl(tipoRuna: 'B' | 'R'): string {
+    // Obtenemos la ruta del background activo
+    const bgUrl = this.boardState.boardBackgroundUrl() || '';
+
+    // NOTA: Ajusta el prefijo 'assets/' si tu carpeta de assets está estructurada distinto.
+    const basePath = 'assets/vector-art/Backgrounds';
+
+    if (bgUrl.includes('Cats')) {
+      return `url('${basePath}/Cats/Rune-${tipoRuna}-Cats.svg')`;
+
+    } else if (bgUrl.includes('Soretro')) {
+      return `url('${basePath}/Soretro/Rune-${tipoRuna}-Soretro.png')`;
+
+    } else {
+      return `url('${basePath}/Classic/Rune-${tipoRuna}-Classic.svg')`;
+    }
+  }
 }
